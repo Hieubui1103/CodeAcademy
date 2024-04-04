6ee7bb48d5fe5c3b1dc7d49d163060ef5666929f
Hieubui1103 <[trunghieubui1103@gmail.com]>
2024-04-04 00:36:55 -0400

    version 1

---
+++ script.js
@@ -0,0 +1,21 @@
+const generateRandomNumber = (num) =>{
+    return Math.random(Math.floor() * num);
+}
+
+const RestaurantMessage = {
+    food: ["pho", "bun dau mam tom", "bun ca'", "xoi gac", "bun cha", "com tam"] ,
+    price: ["$5", "$7", "$10", "$12", "$15"],
+    waitorResponse: [
+        "Is this your final choice?",
+        "Do you want to change your option?",
+        "This price of choice will have a lot of food. Do you want to proceed?",
+        "For your children, I would suggest getting the smaller prices."
+    ]
+}
+
+//version 1.0
+
+    
+
+//"Thank you. We will be right back!"
+//console.log("a");
